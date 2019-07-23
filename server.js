aad33a9c9a76c6704cf732852e4e8eee2b52eacf
Jo <jo.mtz133@gmail.com>
2019-07-23 18:59:42 -0500

    Profile model created, routes to getand update the users profile

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ require('./routes/api/routes')(router);
 
 var usersRoute = require('./routes/api/users');
 var authRoute = require('./routes/api/auth');
+var profileRoute = require('./routes/api/profile');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -22,6 +23,7 @@ app.get('/', (req, res) => res.send('API Running'));
 //app.use(router);
 app.use('/api/auth', authRoute);
 app.use('/api/users', usersRoute);
+app.use('/api/profile', profileRoute);
 
 // Start listening on port
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
